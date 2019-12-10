5bcc89b51420288db2a61bdf3d6c549873ce145d
Wen.Lee <wen.lee@appledaily.com.tw>
2019-12-10 16:36:31 +0800

    Updates

---
+++ precache-manifest.c422b0c7ef1733a5101212ed165f68f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7de2e14ecc5e510af3e7156408bff46c",
+    "url": "/web-apps/index.html"
+  },
+  {
+    "revision": "32ca099860ef2ba42163",
+    "url": "/web-apps/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "b4cc64ecf3856f2bfde8",
+    "url": "/web-apps/static/css/main.fb263b4c.chunk.css"
+  },
+  {
+    "revision": "32ca099860ef2ba42163",
+    "url": "/web-apps/static/js/2.c204cd9e.chunk.js"
+  },
+  {
+    "revision": "b4cc64ecf3856f2bfde8",
+    "url": "/web-apps/static/js/main.a6526ed0.chunk.js"
+  },
+  {
+    "revision": "053a72dad0090338360d",
+    "url": "/web-apps/static/js/runtime-main.c3508105.js"
+  }
+]);
