01f849374fc4e9416ae60acbafc363b4cd7a5a31
Wen.Lee <wen.lee@appledaily.com.tw>
2019-12-10 16:54:54 +0800

    Updates

--- precache-manifest.c079c6730dec91ab17961362d40461b8.js
+++ precache-manifest.c079c6730dec91ab17961362d40461b8.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7de2e14ecc5e510af3e7156408bff46c",
+    "revision": "78bc596519dee755f8c94d6141fab1e7",
     "url": "/web-apps/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/web-apps/static/css/2.b0bb7f2c.chunk.css"
   },
   {
-    "revision": "b4cc64ecf3856f2bfde8",
+    "revision": "e487ed4736f917742cfb",
     "url": "/web-apps/static/css/main.fb263b4c.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/web-apps/static/js/2.c204cd9e.chunk.js"
   },
   {
-    "revision": "b4cc64ecf3856f2bfde8",
-    "url": "/web-apps/static/js/main.a6526ed0.chunk.js"
+    "revision": "e487ed4736f917742cfb",
+    "url": "/web-apps/static/js/main.299e1792.chunk.js"
   },
   {
     "revision": "053a72dad0090338360d",
